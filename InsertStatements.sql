e3f2f82b70889c9193848455bfbc94279fcedacd
Francisco Alcaraz <falcatraz99@gmail.com>
2023-05-07 17:13:29 -0700

    finished tables and added inserts

---
+++ InsertStatements.sql
@@ -0,0 +1,25 @@
+-- this is for inserting values into our tables
+use publicworks;
+-- Inserting into the job title table
+INSERT INTO job_title (j_titleid, j_titlename, j_hourlyrate) VALUES 
+(1000, "Admin Assistant", 29.95),
+(1001, "Civil Engineer", 45.67),
+(1002, "Engineer Tech", 32.09),
+(1003, "Student Worker", 15.75),
+(1004, "Contract Specialist", 30.01);
+
+SELECT * FROM job_title;
+
+-- inserting into departments
+INSERT INTO department (d_departmentid, d_departmentname, d_departmentphone, d_departmentlocation) VALUES
+(200, "Sewers Department", "786-901-9312", "Hall of Administration"),
+(201, "Watershed Department", "786-899-6310", "Saticoy Extension"),
+(202, "Bridges Department", "786-890-6230", "Oxnard PWA Building"),
+(203, "Roads & Transportation", "786-432-5746", "Hall of Administration"),
+(204, "Operations & Maintenance", "786-334-9090", "Moorpark Extension");
+
+SELECT * FROM department;
+
+-- inserting into contractor
+INSERT INTO contractor (c_contractorid, c_contractorname, c_contact, c_contractorphoneno) VALUES
+(400, "HAP LLC", "Humbert Gonzales", "705-324-8888");
--- ProjectDataBaseCreation.sql
+++ ProjectDataBaseCreation.sql
@@ -2,24 +2,133 @@ CREATE DATABASE PublicWorks;
 
 USE PublicWorks;
 
+-- final job title table
+CREATE TABLE job_title (
+	j_titleID INT PRIMARY KEY,
+    j_titleName VARCHAR(25),
+    j_hourlyRate FLOAT
+    );
+
+-- final department table
+CREATE TABLE department (
+	d_departmentID INT PRIMARY KEY,
+    d_departmentName VARCHAR(20),
+    d_departmentPhone VARCHAR(13),
+    d_departmentLocation VARCHAR(25)
+);
+    
+-- final contractor table
+CREATE TABLE contractor (
+	c_contractorID INT PRIMARY KEY,
+	c_contractorName VARCHAR(20),
+	c_contact VARCHAR(40),
+	c_contractorPhoneNo VARCHAR(13)
+);
+
+-- final employee table
 CREATE TABLE employee (
-	e_id INT PRIMARY KEY,
+	e_employeeID INT PRIMARY KEY,
     e_firstname VARCHAR(20),
     e_lastname VARCHAR(20),
     e_phonenumber VARCHAR(13),
-    e_title VARCHAR(30)
+    titleID INT, -- foreign key
+    departmentNo INT, -- foreign key
+    
+    CONSTRAINT fk_titleid FOREIGN KEY (titleID) REFERENCES job_title(j_titleID),
+    CONSTRAINT fk_departno FOREIGN KEY (departmentNo) REFERENCES department(d_departmentID)
 );
 
-CREATE TABLE contractor (
-	c_contractorID INT PRIMARY KEY,
-    c_contractorName VARCHAR(20),
-    c_contactName VARCHAR(40),
-    c_contractorPhoneNo VARCHAR(13)
-);
-
+-- final zone table
 CREATE TABLE zone (
 	z_zoneNo INT PRIMARY KEY,
     z_zoneName VARCHAR(20),
-    z_zoneSupervisorID INT
+    zoneSupervisorID INT, -- foreign key
+    CONSTRAINT fk_zoneemp FOREIGN KEY (zoneSupervisorID) REFERENCES employee(e_employeeID)
+	);
+
+-- final construction contract table
+CREATE TABLE construction_contract (
+	ct_contractNo INT, -- primary key
+    contractorID INT, -- foreign key and primary key
+	countyRepID INT, -- foreign key and primary key
+    ct_contractDate DATE,
+    ct_awardAmount float,
+    
+    CONSTRAINT pk_contract PRIMARY KEY (ct_contractNo, contractorID, countyRepID),
+    CONSTRAINT fk_ctcontractor FOREIGN KEY (contractorID) REFERENCES contractor(c_contractorID),
+    CONSTRAINT fk_ctemp FOREIGN KEY (countyRepID) REFERENCES employee(e_employeeID)
 );
 
+-- final county bank account table
+CREATE TABLE county_bank_account (
+	b_accountNo INT PRIMARY KEY,
+    b_accountName VARCHAR(30),
+    b_dateOpened DATE,
+    b_accountBalance FLOAT,
+    b_PurposeDescription VARCHAR (70),
+    accountOwnerID INT, -- foreign key
+    
+    CONSTRAINT fk_ownerid FOREIGN KEY (accountOwnerID) REFERENCES department(d_departmentID)
+);
+
+-- final invoice table
+CREATE TABLE invoice (
+	i_invoiceNo INT PRIMARY KEY,
+    i_amount FLOAT,
+    i_invoiceDate DATE,
+    bankAccountNo INT, -- foreing key
+    contractID INT, -- foreign key
+    
+    CONSTRAINT fk_invacctno FOREIGN KEY (bankAccountNo) REFERENCES county_bank_account(b_accountNo),
+    CONSTRAINT fk_invcontract FOREIGN KEY (contractID) REFERENCES construction_contract(ct_contractNo)
+);
+
+-- final project table
+CREATE TABLE project (
+	p_projectNo INT PRIMARY KEY,
+    p_projectName VARCHAR (40),
+    p_startDate DATE,
+    p_endDate DATE,
+    zoneNo INT, -- foreign key
+    contractNo INT, -- foreign key
+    
+    CONSTRAINT fk_projzone FOREIGN KEY (zoneNo) REFERENCES zone(z_zoneNo),
+    CONSTRAINT fk_projcont FOREIGN KEY (contractNo) REFERENCES construction_contract(ct_contractNo)
+);
+
+-- final instpection table
+CREATE TABLE inspection (
+	IN_InspectionNo INT PRIMARY KEY,
+    IN_inspectionDate DATE,
+    projectNo INT, -- foreign key
+    inspectorID INT, -- foreign key
+    
+    CONSTRAINT fk_inspproj FOREIGN KEY (projectNo) REFERENCES project(p_projectNo),
+    CONSTRAINT fk_inspector FOREIGN KEY (inspectorID) REFERENCES employee(e_employeeID)
+);
+
+-- final violation table
+CREATE TABLE violation (
+	v_violationNo INT, -- PK
+    projectNo INT, -- pk, fk
+    inspectionNo INT, -- pk, fk
+    v_violationDate DATE,
+    v_fineAmount FLOAT,
+    v_reason VARCHAR (70),
+    
+    CONSTRAINT pk_violation PRIMARY KEY (v_violationNo, projectNo, inspectionNo),
+    CONSTRAINT fk_vioproj FOREIGN KEY (projectNo) REFERENCES project(p_projectNo),
+    CONSTRAINT fk_vioinsp FOREIGN KEY (inspectionNo) REFERENCES inspection(IN_InspectionNo)
+);
+
+-- final permit table
+CREATE TABLE permit (
+	pt_permitNo INT, -- PK,
+    projectNo INT, -- PK, FK
+    pt_permitName VARCHAR(35),
+    pt_permitDate DATE,
+    
+    CONSTRAINT pk_permit PRIMARY KEY (pt_permitNo, projectNo),
+    CONSTRAINT fk_permproj FOREIGN KEY (projectNo) REFERENCES project(p_projectNo)
+);
+    
